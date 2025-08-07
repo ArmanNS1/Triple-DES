9292d7ebeb7d3839ff4241ddeba81aaec1694096
Arman <a.n004@yahoo.com>
2025-08-07 11:23:45 +0330

    Update CTRModeHandler.cs

--- 3DES/ModeHandlers/CTRModeHandler.cs
+++ 3DES/ModeHandlers/CTRModeHandler.cs
@@ -12,25 +12,19 @@ namespace DES.ModeHandlers
             ValidateBlockSize(plaintext);
             var result = new byte[plaintext.Length];
 
-            // Initialize counter with IV
             BitArray counter = new BitArray(InitializationVector);
 
             for (int i = 0; i < plaintext.Length; i += 8)
             {
-                // Encrypt counter to generate keystream
                 BitArray keystream = DESEncryption.EncryptBlock(counter, RoundKeys);
 
-                // Get current plaintext block
                 BitArray plaintextBlock = ConvertToBitArray(plaintext, i);
 
-                // XOR keystream with plaintext
                 BitArray ciphertextBlock = new BitArray(keystream);
                 ciphertextBlock.Xor(plaintextBlock);
 
-                // Store the result
                 CopyBitArrayToBytes(ciphertextBlock, result, i);
 
-                // Increment counter for next block
                 IncrementCounter(counter);
             }
 
@@ -39,13 +33,11 @@ namespace DES.ModeHandlers
 
         public override byte[] Decrypt(byte[] ciphertext)
         {
-            // CTR decryption is identical to encryption
             return Encrypt(ciphertext);
         }
 
         private static void IncrementCounter(BitArray counter)
         {
-            // Treat the last 32 bits as a counter
             bool carry = true;
             for (int i = counter.Length - 1; i >= counter.Length - 32 && carry; i--)
             {
